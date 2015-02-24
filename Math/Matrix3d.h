5b89dd3b5239bab268f4889d4c5c5a283e97d68c
SatoshiMabuchi <mabuchi@premiumgraphics.co.jp>
2015-02-24 14:37:22 +0900

    Matrix3d　をヘッダファイルのみに修正．

--- Math/Matrix3d.h
+++ Math/Matrix3d.h
@@ -36,21 +36,21 @@ public:
 
 	void setRotateX( const float angle ) { *this = RotateX( angle ); }
 
-	void setRotateY(const float angle) {
-		x00 = ::cos(angle);
-		x01 = 0.0f;
-		x02 = ::sin(angle);
+	void setRotateY(const float angle) { *this = RotateZ( angle ); }
 
-		x10 = 0.0f;
-		x11 = 1.0f;
+	void setRotateZ(const float angle) {
+		x00 = ::cos(angle);
+		x01 = -::sin(angle);
+		x02 = 0.0f;
+
+		x10 = ::sin(angle);
+		x11 = ::cos(angle);
 		x12 = 0.0f;
 
-		x20 = -::sin(angle);
+		x20 = 0.0f;
 		x21 = 0.0f;
-		x22 = ::cos(angle);
+		x22 = 1.0f;
 	}
-
-	void setRotateZ( const float angle );
 
 	static Matrix3d Identity() {
 		return Matrix3d(
@@ -59,7 +59,12 @@ public:
 			0.0f, 0.0f, 1.0f );
 	}
 
-	static Matrix3d Zero();
+	static Matrix3d Zero() {
+		return Matrix3d(
+			0.0f, 0.0f, 0.0f,
+			0.0f, 0.0f, 0.0f,
+			0.0f, 0.0f, 0.0f);
+	}
 
 	static Matrix3d RotateX( const float angle ) {
 		return Matrix3d(
@@ -69,9 +74,12 @@ public:
 	}
 
 	static Matrix3d RotateY( const float angle ) {
-		Matrix3d matrix;
-		matrix.setRotateY( angle );
-		return matrix;
+		return Matrix3d
+			(
+			::cos(angle), 0.0f,::sin(angle),
+			0.0f, 1.0f, 0.0f,
+			-::sin(angle), 0.0f, ::cos(angle)
+			);
 	}
 
 	static Matrix3d RotateZ( const float angle ) {
@@ -80,9 +88,15 @@ public:
 		return matrix;
 	}
 
-	bool isZero() const;
+	bool isZero() const { return this->equals(Matrix3d::Zero()); }
 	
-	bool isIdentity() const;
+	bool isIdentity() const {
+		Matrix3d identityMatrix(
+			1.0, 0.0, 0.0,
+			0.0, 1.0, 0.0,
+			0.0, 0.0, 1.0);
+		return this->equals(identityMatrix);
+	}
 
 	bool equals( const Matrix3d& rhs ) const {
 		return
@@ -97,11 +111,40 @@ public:
 			Tolerances::isEqualStrictly( x22, rhs.x22 );
 	}
 
-	float getDeterminant() const;
+	float getDeterminant() const {
+		return
+			x00 * x11 * x22
+			+ x02 * x10 * x21
+			+ x20 * x01 * x12
+			- x02 * x11 * x20
+			- x00 * x12 * x21
+			- x01 * x10 * x22;
+	}
 
-	Matrix3d getInverse() const;
+	Matrix3d getInverse() const {
+		const float denominator = getDeterminant();
+		assert(!Tolerances::isEqualStrictly(denominator));
 
-	Matrix3d product( const Matrix3d& rhs );
+		const float i00 = x11 * x22 - x12 * x21;
+		const float i01 = x21 * x02 - x22 * x01;
+		const float i02 = x01 * x12 - x02 * x11;
+
+		const float i10 = x12 * x20 - x10 * x22;
+		const float i11 = x22 * x00 - x20 * x02;
+		const float i12 = x02 * x10 - x00 * x12;
+
+		const float i20 = x10 * x21 - x11 * x20;
+		const float i21 = x20 * x01 - x21 * x00;
+		const float i22 = x00 * x11 - x01 * x10;
+
+		Matrix3d matrix(i00, i01, i02,
+			i10, i11, i12,
+			i20, i21, i22);
+		matrix.scale(1.0f / denominator);
+		return matrix;
+	}
+
+	Matrix3d product(const Matrix3d& rhs) { return *this = getProduct(rhs); }
 
 	Matrix3d getProduct( const Matrix3d& rhs ) const {
 		return Matrix3d( 
@@ -116,9 +159,17 @@ public:
 				x20 * rhs.x02 + x21 * rhs.x12 + x22 * rhs.x22);
 	}
 
-	Matrix3d scale( const float factor );
+	Matrix3d scale(const float factor) {
+		x00 *= factor; x01 *= factor; x02 *= factor;
+		x10 *= factor; x11 *= factor; x12 *= factor;
+		x20 *= factor; x21 *= factor; x22 *= factor;
+		return *this;
+	}
 
-	Matrix3d getScaled( const float factor ) const;
+	Matrix3d getScaled(const float factor) const {
+		Matrix3d matrix = *this;
+		return matrix.scale(factor);
+	}
 
 	Matrix3d add( const Matrix3d& rhs ) {
 		x00 += rhs.x00;
@@ -136,21 +187,24 @@ public:
 		return *this;
 	}
 
-	Matrix3d getAdd( const Matrix3d& rhs ) const;
+	Matrix3d getAdd(const Matrix3d& rhs) const {
+		Matrix3d matrix = *this;
+		return matrix.add(rhs);
+	}
 
 	bool operator==(const Matrix3d& rhs) const { return this->equals(rhs); }
 
 	Matrix3d operator+(const Matrix3d& rhs) const { return getAdd(rhs); }
 
-	const Matrix3d operator+=(const Matrix3d& rhs);
+	const Matrix3d operator+=(const Matrix3d& rhs) { return add(rhs); }
 
-	Matrix3d operator-( const Matrix3d& rhs ) const;
+	Matrix3d operator-(const Matrix3d& rhs) const { return getAdd(rhs.getScaled(-1.0)); }
 	
-	const Matrix3d operator-=( const Matrix3d& rhs );
+	const Matrix3d operator-=(const Matrix3d& rhs) { return add(rhs.getScaled(-1.0)); }
 	
-	Matrix3d operator*( const Matrix3d& rhs ) const;
+	Matrix3d operator*(const Matrix3d& rhs) const { return getProduct(rhs); }
 	
-	const Matrix3d operator*=( const Matrix3d& rhs );
+	const Matrix3d operator*=(const Matrix3d& rhs) { return product(rhs); }
 
 	float getX00() const { return x00; }
 
