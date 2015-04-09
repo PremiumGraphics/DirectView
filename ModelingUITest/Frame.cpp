405f1afd2f57e6e431e9baf53e929b1e397ac9eb
SatoshiMabuchi <mabuchi@premiumgraphics.co.jp>
2015-04-09 19:36:15 +0900

    不要なファイルを削除．

--- ModelingUITest/Frame.cpp
+++ ModelingUITest/Frame.cpp
@@ -300,3 +300,24 @@ void Frame::OnCapture( wxRibbonButtonBarEvent& e )
 	image.SaveFile(filename, wxBITMAP_TYPE_BMP);
 
 }
+
+
+void Model::setSelectedVertex(const unsigned int id)
+{
+	if (id >= 0 && id < getVertices().size()) {
+		const VertexSPtrVector& vs = getVertices();
+		const VertexSPtr& v = vs[id];
+		this->selectedVertex.clear();
+		this->selectedVertex.push_back(v);
+	}
+}
+
+void Model::setSelectedFace(const unsigned int id)
+{
+	if (id >= 0 && id < getFaces().size()) {
+		const FaceSPtrVector& faces = getFaces();
+		const FaceSPtr& f = faces[id];
+		this->selectedFace.clear();
+		this->selectedFace.push_back(f);
+	}
+}
--- ModelingUITest/Frame.h
+++ ModelingUITest/Frame.h
@@ -14,6 +14,64 @@
 
 #include <memory>
 
+
+#ifndef __CRYSTAL_MODELING_MODEL_H__
+#define __CRYSTAL_MODELING_MODEL_H__
+
+#include "../Graphics/PolygonBuilder.h"
+#include "../Graphics/Light.h"
+#include "../Graphics/Camera.h"
+
+namespace Crystal {
+	namespace Modeling {
+
+		class Model {
+		public:
+			Model() :
+				builder(new Graphics::PolygonBuilder())
+			{
+				camera.setNear(1.0f);
+			}
+
+			void clear()
+			{
+				builder->clear();
+			}
+
+			Graphics::PolygonBuilderSPtr getPolygonBuilder() const  { return builder; }
+
+			Graphics::FaceSPtrVector getFaces() const { return builder->getFaces(); }
+
+			Graphics::PolygonSPtrList getPolygons() const { return builder->getPolygons(); }
+
+			Graphics::VertexSPtrVector getVertices() const { return builder->getVertices(); }
+
+			Graphics::VertexSPtrVector getSelectedVertices() const { return selectedVertex; }
+
+			Graphics::FaceSPtrVector getSelectedFaces() const { return selectedFace; }
+
+			void setSelectedVertex(const unsigned int id);
+
+			void setSelectedFace(const unsigned int id);
+
+			Graphics::Camera<float>* getCamera() { return &camera; }
+
+
+
+		private:
+			const Graphics::PolygonBuilderSPtr builder;
+			Graphics::VertexSPtrVector selectedVertex;
+			Graphics::FaceSPtrVector selectedFace;
+
+			Graphics::Camera<float> camera;
+
+		};
+
+	}
+}
+
+#endif
+
 namespace Crystal {
 	namespace Modeling {
 		class View;
@@ -25,6 +83,9 @@ namespace Crystal {
 		class LightProperty;
 
 		class Frame;
+		class Model;
+
+
 
 class Widgets {
 public:
