de582436190416e818405f3199d49072a0480e86
SatoshiMabuchi <mabuchi@premiumgraphics.co.jp>
2015-04-09 18:09:31 +0900

    共通UI のためのライブラリを作成．

--- ModelingUITest/ModelingDialog.cpp
+++ ModelingUITest/ModelingDialog.cpp
@@ -3,34 +3,6 @@
 
 using namespace Crystal::Modeling;
 
-TriangleConfigDialog::TriangleConfigDialog(wxWindow* parent) :
-		wxDialog(parent, wxID_ANY, "QuadConfig", wxDefaultPosition, wxSize(500, 500))
-{
-	new wxStaticText(this, wxID_ANY, "X Size", wxPoint(0, 100));
-	xSize = new wxSpinCtrlDouble(this, wxID_ANY, wxEmptyString, wxPoint(100, 100));
-	xSize->SetValue(1.0);
-
-	new wxStaticText(this, wxID_ANY, "Y Size", wxPoint(0, 200));
-	ySize = new wxSpinCtrlDouble(this, wxID_ANY, wxEmptyString, wxPoint(100, 200));
-	ySize->SetValue(1.0);
-
-	new wxButton(this, wxID_OK, "OK", wxPoint(300, 100));
-	new wxButton(this, wxID_CANCEL, "Cancel", wxPoint(300, 200));
-}
-
-void TriangleConfigDialog::setConfig(const Config& config)
-{
-	xSize->SetValue(config.xSize);
-	ySize->SetValue(config.ySize);
-}
-
-TriangleConfigDialog::Config TriangleConfigDialog::getConfig() const
-{
-	Config config;
-	config.xSize = xSize->GetValue();
-	config.ySize = ySize->GetValue();
-	return config;
-}
 
 QuadConfigDialog::QuadConfigDialog(wxWindow* parent) :
 wxDialog(parent, wxID_ANY, "QuadConfig", wxDefaultPosition, wxSize(500, 500))
--- ModelingUITest/ModelingDialog.h
+++ ModelingUITest/ModelingDialog.h
@@ -5,29 +5,11 @@
 
 #include "../Math/Cone.h"
 
+#include "../UI/UI.h"
+
 namespace Crystal{
 	namespace Modeling {
 
-class TriangleConfigDialog : public wxDialog
-{
-public:
-	struct Config {
-		Config() : xSize(1.0), ySize( 1.0)
-		{}
-		float xSize;
-		float ySize;
-	};
-
-	TriangleConfigDialog(wxWindow* parent);
-
-	void setConfig(const Config& config);
-
-	Config getConfig() const;
-
-private:
-	wxSpinCtrlDouble* xSize;
-	wxSpinCtrlDouble* ySize;
-};
 
 class QuadConfigDialog : public wxDialog
 {
