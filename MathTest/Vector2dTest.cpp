fe6c8458661c314d2b119850833260dea1ca2ab5
SatoshiMabuchi <mabuchi@premiumgraphics.co.jp>
2015-05-08 11:38:53 +0900

    ワーニングに対処．

--- MathTest/Vector2dTest.cpp
+++ MathTest/Vector2dTest.cpp
@@ -20,23 +20,64 @@ TYPED_TEST( Vector2dTest, ScaleTest )
 	EXPECT_EQ( Vector2d<TypeParam>( 2.0, 2.0 ), scaled );
 }
  
-TYPED_TEST( Vector2dTest, SqrtTest )
+TEST( Vector2dTest, TestGetLengthFloat )
 {
-	Vector2d<TypeParam> v( 1.0, 1.0 );
-	//EXPECT_FALSE( Tolerances::isEqualLoosely( v.getLength(), std::sqrt( 1.99 ) ) );
+	Vector2d<float> v( 1.0f, 1.0f );
 
-	EXPECT_FLOAT_EQ( v.getLength(), std::sqrt( 2.0f ) );
-	EXPECT_FLOAT_EQ( v.getLength(), std::sqrt( 2.0f ) );
+	const auto actual = v.getLength();
+	const auto expected = std::sqrt(2.0f);
 
-	EXPECT_FLOAT_EQ( v.getLengthSquared(), 2.0f );
-	EXPECT_FLOAT_EQ( v.getLengthSquared(), 2.0f );
-
+	EXPECT_FLOAT_EQ( expected, expected );
+	EXPECT_FLOAT_EQ( expected, expected );
 }
- 
-TYPED_TEST( Vector2dTest, LengthTest )
+
+TEST(Vector2dTest, TestGetLengthDouble)
 {
-	Vector2d<TypeParam> v0( 1.0f, 1.0f );
-	Vector2d<TypeParam> v1( 2.0f, 2.0f );
-	EXPECT_FLOAT_EQ( v0.getDistance( v1 ), std::sqrt( 2.0f ) );
-	EXPECT_FLOAT_EQ( v0.getDistance( v1 ), std::sqrt( 2.0f ) );
+	Vector2d<double> v(1.0f, 1.0f);
+
+	const auto actual = v.getLength();
+	const auto expected = std::sqrt(2.0);
+
+	EXPECT_DOUBLE_EQ(expected, expected);
+}
+
+
+TEST(Vector2dTest, TestGetLengthSquaredFloat)
+{
+	Vector2d<float> v(1.0, 1.0);
+
+	const auto actual = v.getLengthSquared();
+	const auto expected = 2.0f;
+
+	EXPECT_FLOAT_EQ( expected, actual );
+}
+
+TEST(Vector2dTest, TestGetLengthSquaredDouble)
+{
+	Vector2d<double> v(1.0, 1.0);
+
+	const auto actual = v.getLengthSquared();
+	const auto expected = 2.0;
+
+	EXPECT_DOUBLE_EQ( expected, actual );
+}
+
+TEST(Vector2dTest, TestGetDistanceFloat)
+{
+	Vector2d<float> v0(1.0f, 1.0f);
+	Vector2d<float> v1(2.0f, 2.0f);
+	const auto expected = std::sqrt(2.0f);
+
+	EXPECT_FLOAT_EQ(expected, v0.getDistance(v1));
+	EXPECT_FLOAT_EQ(expected, v1.getDistance(v0));
+}
+
+TEST(Vector2dTest, TestGetDistanceDouble)
+{
+	Vector2d<double> v0(1.0f, 1.0f);
+	Vector2d<double> v1(2.0f, 2.0f);
+	const auto expected = std::sqrt(2.0);
+
+	EXPECT_DOUBLE_EQ(expected, v0.getDistance(v1));
+	EXPECT_DOUBLE_EQ(expected, v1.getDistance(v0));
 }
