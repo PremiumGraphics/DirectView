b04dc93aded2bdd9b59396ceed9a3aa698e17339
SatoshiMabuchi <mabuchi@premiumgraphics.co.jp>
2015-05-20 17:05:50 +0900

    MarchingSquare用のテーブルを一通り作成．

--- Math/Marching2d.h
+++ Math/Marching2d.h
@@ -21,10 +21,8 @@ public:
 		if (bmp == table[0]) {
 		}
 		else if (bmp == table[1]) {
-			lines.push_back( Line2d<T>( Position2d<T>( 0.5f, 0.0f ), Position2d<T>( 0.0f, 0.5f ) ) );
 		}
 		else if (bmp == table[2]) {
-			lines.push_back( Line2d<T>(Position2d<float>(0.0f, 0.5f), Position2d<T>(1.0f, 0.5f)));
 		}
 		/*if (bmp.get(0, 0)) {
 
@@ -35,16 +33,52 @@ public:
 
 	std::array< Bitmap2x2, 16 > getTables() const { return table; }
 
+	std::array< std::vector< Line2d<T> >, 16 > getLineTable() const { return lineTable; }
+
 	void buildTable() {
 		for (size_t i = 0; i < 16; ++i) {
 			table[i] = Bitmap2x2(i);
 		}
+
+		const Position2d<T> left(0.0, 0.5f);
+		const Position2d<T> right(1.0f, 0.5f);
+
+		const Position2d<T> bottom(0.5, 0.0f);
+		const Position2d<T> top(0.5, 1.0f);
+
+		/*
+		std::array <
+			std::array < Position2d<T>, 2 >,
+			2 >
+		> positions;
+		positions[0][0] = xminus;
+		positions[1][0] = x;
+		*/
+
+		lineTable[0] = {};
+		lineTable[1] = { Line2d<T>(left, bottom) };
+		lineTable[2] = { Line2d<T>(bottom, right) };
+		lineTable[3] = { Line2d<T>(left, right) };
+		lineTable[4] = { Line2d<T>(top, right) };
+		lineTable[5] = { Line2d<T>(left, top), Line2d<T>(bottom, right) };
+		lineTable[6] = { Line2d<T>(bottom, top) };
+		lineTable[7] = { Line2d<T>(left, top) };
+		lineTable[8] = lineTable[7];
+		lineTable[9] = lineTable[6];
+		lineTable[10] = { Line2d<T>(left, bottom), Line2d<T>(top, right) };
+		lineTable[11] = lineTable[4];
+		lineTable[12] = lineTable[3];
+		lineTable[13] = lineTable[2];
+		lineTable[14] = lineTable[1];
+		lineTable[15] = lineTable[0];
 	}
 
 private:
 	std::vector<Line2d<T> > lines;
 
 	std::array< Bitmap2x2, 16 > table;
+
+	std::array< std::vector< Line2d<T> >, 16 > lineTable;
 };
 	}
 }
