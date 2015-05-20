4c836774bedca57b562e3f1cc54a34dd6d8674a2
SatoshiMabuchi <mabuchi@premiumgraphics.co.jp>
2015-05-20 15:50:09 +0900

    Vector を汎用化．

--- Math/Vector.h
+++ Math/Vector.h
@@ -6,6 +6,7 @@
 namespace Crystal {
 	namespace Math {
 
+		/*
 template< typename T, size_t DIM >
 class Vector final
 {
@@ -29,6 +30,156 @@ public:
 
 public:
 	std::array< T, DIM > v;
+};
+*/
+
+
+template< typename T, size_t DIM >
+class Vector final
+{
+public:
+
+	Vector(void)
+	{
+		v.fill(0);
+	}
+
+	Vector(const T x, const T y)
+	{
+		v = { x, y };
+	}
+
+	Vector(const Vector& start, const Vector& end)
+	{
+		v = {
+			end.getX() - start.getX(),
+			end.getY() - start.getY()
+		};
+	}
+
+	static Vector Zero() { return Vector(0.0f, 0.0f); }
+
+	static Vector UnitX() { return Vector(1.0f, 0.0f); }
+
+	static Vector UnitY() { return Vector(0.0f, 1.0f); }
+
+	T getLengthSquared() const {
+		T ls = 0;
+		for (auto x : v) {
+			ls += x * x;
+		}
+		return ls;
+	}
+
+	T getLength() const {
+		return ::sqrt(getLengthSquared());
+	}
+
+	T getDistance(const Vector& rhs) const {
+		return ::sqrt(getDistanceSquared(rhs));
+	}
+
+	T getDistanceSquared(const Vector& rhs) const {
+		T ls = 0;
+		for (size_t i = 0; i < DIM; ++i) {
+			ls += pow(v[i] - rhs.v[i], 2);
+		}
+		return ls;
+	}
+
+	Vector scale(const T factor) {
+		for (auto& x : v) {
+			x *= factor;
+		}
+		return *this;
+	}
+
+	Vector scale(const Vector factor) {
+		for (size_t i = 0 i < DIM; ++i) {
+			v[i] *= factor[i];
+		}
+		return *this;
+	}
+
+	Vector getScaled(const T factor) const {
+		return Vector(getX() * factor, getY() * factor);
+	}
+
+	Vector normalize() {
+		const T length = getLength();
+		x /= length;
+		y /= length;
+		return *this;
+	}
+
+	Vector getNormalized() const {
+		Vector<T> vector = *(this);
+		return vector.normalize();
+	}
+
+	bool isNormalized() const {
+		return Tolerance<T>::isEqualLoosely(getLength(), 1.0);
+	}
+
+	bool equals(const Vector&rhs) const {
+		return Tolerance<T>::isEqualLoosely(getDistanceSquared(rhs));
+	}
+
+	bool operator==(const Vector& rhs) const {
+		return equals(rhs);
+	}
+
+	bool operator!=(const Vector& rhs) const {
+		return !equals(rhs);
+	}
+
+	Vector operator+(const Vector& rhs) const {
+		return Vector<T>(x + rhs.x, y + rhs.y);
+	}
+
+	Vector operator-(const Vector& rhs) const {
+		return Vector(x - rhs.x, y - rhs.y);
+	}
+
+	Vector operator+=(const Vector& rhs) {
+		x += rhs.x;
+		y += rhs.y;
+		return *this;
+	}
+
+	Vector operator-=(const Vector& rhs) {
+		x -= rhs.x;
+		y -= rhs.y;
+		return *this;
+	}
+
+	Vector operator*(const T factor) const {
+		Vector<T> vector(*this);
+		return vector.scale(factor);
+	}
+
+	Vector operator/(const T factor) const {
+		Vector<T> vector(*this);
+		return vector.scale(1.0f / factor);
+	}
+
+	Vector operator*=(const T factor) { return scale(factor); }
+
+	Vector operator/=(const T factor) { return scale(1.0f / factor); }
+
+public:
+	T getX() const { return v[0]; }
+
+	T getY() const { return v[1]; }
+
+	void setX(const T x) { this->v[0] = x; }
+
+	void setY(const T y) { this->v[1] = y; }
+
+	std::array<T, DIM> toArray() const { return v; }
+
+private:
+	std::array<T, DIM> v;
 };
 
 	}
--- Math/Vector2d.h
+++ Math/Vector2d.h
@@ -6,150 +6,15 @@
 #include <array>
 #include "Tolerance.h"
 
+#include "Vector.h"
+
 namespace Crystal{
 	namespace Math{
 
 
-template< typename T >
-class Vector2d final
-{
-public:
-	
-	Vector2d(void) :
-		x( 0.0 ), y( 0.0 )
-	{}
 
-	Vector2d( const T x, const T y ) :
-		x( x), y( y)
-	{}
-
-
-	Vector2d( const Vector2d& start, const Vector2d& end ) :
-		x( end.x - start.x ),
-		y( end.y - start.y )
-	{}
-
-	static Vector2d Zero() { return Vector2d( 0.0f, 0.0f ); }
-
-	static Vector2d UnitX() { return Vector2d( 1.0f, 0.0f ); }
-
-	static Vector2d UnitY() { return Vector2d( 0.0f, 1.0f ); }
-
-	T getLengthSquared() const {
-		return x * x + y * y;
-	}
-
-	T getLength() const {
-		return ::sqrt( getLengthSquared() );
-	}
-	
-	T getDistance( const Vector2d& rhs ) const {
-		return ::sqrt( getDistanceSquared( rhs ) );
-	}
-
-	T getDistanceSquared( const Vector2d& rhs ) const {
-		return pow( x - rhs.x, 2 ) + pow( y - rhs.y, 2 );
-	}
-
-	Vector2d scale( const T factor ) {
-		x *= factor;
-		y *= factor;
-		return *this;
-	}
-
-	Vector2d scale( const T xFactor, const T yFactor ) {
-		x *= xFactor;
-		y *= yFactor;
-		return *this;
-	}
-
-	Vector2d getScaled( const T factor ) const {
-		return Vector2d( x * factor, y * factor );
-	}
-
-	Vector2d normalize() {
-		const T length = getLength();
-		x /= length;
-		y /= length;
-		return *this;
-	}
-
-	Vector2d getNormalized() const {
-		Vector2d<T> vector = *(this);
-		return vector.normalize();
-	}
-
-	bool isNormalized() const {
-		return Tolerance<T>::isEqualLoosely( getLength(), 1.0 );
-	}
-
-	bool equals( const Vector2d&rhs ) const {
-		return Tolerance<T>::isEqualLoosely( getDistanceSquared( rhs ) );
-	}
-
-	bool operator==( const Vector2d& rhs ) const {
-		return equals( rhs );
-	}
-
-	bool operator!=(const Vector2d& rhs) const {
-		return !equals( rhs );
-	}
-
-	Vector2d operator+(const Vector2d& rhs) const {
-		return Vector2d<T>( x + rhs.x, y + rhs.y );
-	}
-
-	Vector2d operator-(const Vector2d& rhs) const {
-		return Vector2d( x - rhs.x, y - rhs.y );
-	}
-
-	Vector2d operator+=(const Vector2d& rhs) {
-		x += rhs.x;
-		y += rhs.y;
-		return *this;
-	}
-
-	Vector2d operator-=(const Vector2d& rhs) {
-		x -= rhs.x;
-		y -= rhs.y;
-		return *this;
-	}
-
-	Vector2d operator*(const T factor) const {
-		Vector2d<T> vector( *this );
-		return vector.scale( factor );
-	}
-
-	Vector2d operator/(const T factor) const {
-		Vector2d<T> vector( *this );
-		return vector.scale( 1.0f / factor );
-	}
-	
-	Vector2d operator*=( const T factor ) { return scale( factor ); }
-
-	Vector2d operator/=( const T factor ) { return scale( 1.0f / factor ); }
-
-public:
-	T getX() const { return x; }
-
-	T getY() const { return y; }
-
-	void setX( const T x ) { this->x = x; }
-
-	void setY( const T y ) { this->y = y; }
-
-	std::array<T, 2> toArray() const {
-		std::array<T, 2> val;
-		val[0] = x;
-		val[1] = y;
-		return val;
-	}
-
-private:
-	T x;
-	T y;
-};
-
+template<typename T>
+using Vector2d = Vector<T,2>;
 
 template< typename T >
 static Vector2d<T> operator*( float factor, const Vector2d<T>& rhs ) { return rhs.getScaled( factor ); }
