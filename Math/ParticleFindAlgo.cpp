5189dc85a759e38e24fbf127b591b14e33133fdd
SatoshiMabuchi <mabuchi@premiumgraphics.co.jp>
2015-05-20 11:14:18 +0900

    Math を削除．ヘッダファイルのみになったため．

--- Math/ParticleFindAlgo.cpp
+++
@@ -1,9 +0,0 @@
-#include "../Particle/ParticleFindAlgo.h"
-
-using namespace Crystal::Math;
-using namespace Crystal::Particle;
-
-bool CollisionFindAlgo::hasCollision(const ParticleBasePair& pair, const float threshold)
-{
-	return pair.getDistance() < threshold;
-}
