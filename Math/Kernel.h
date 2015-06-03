9665fb27a60776e895b16376e44e86acb3fbcc1f
SatoshiMabuchi <mabuchi@premiumgraphics.co.jp>
2015-06-03 16:44:17 +0900

    ガウス関数を多次元化．

--- Math/Kernel.h
+++ Math/Kernel.h
@@ -8,24 +8,32 @@ namespace Crystal {
 	namespace Math {
 
 template<typename T>
-class Gaussian1d {
+class Gaussian {
 public:
-	Gaussian1d() :
+	Gaussian() :
 		average(0),
 		distribution(1)
 	{}
 
-	Gaussian1d(const T average, const T distribution) :
+	Gaussian(const T average, const T distribution) :
 		average( average ),
 		distribution( distribution )
 	{}
 
-	~Gaussian1d() = default;
+	~Gaussian() = default;
 
 	T get(const T x) {
-		const auto coe = 1.0 / std::sqrt(2.0 * Tolerance<T>::getPI() * distribution * distribution);
-		const auto exp_ = -std::pow((x - average), 2) / (2.0 * distribution);
+		const auto coe = T(1) / std::sqrt(T(2) * Tolerance<T>::getPI() * distribution * distribution);
+		const auto exp_ = -std::pow((x - average), 2) / (T(2) * distribution);
 		return coe * std::exp(exp_);
+	}
+
+	T get(const T x, const T y){
+		return get(x) + get(y);
+	}
+
+	T get(const T x, const T y, const T z) {
+		return get(x) + get(y) + get(z);
 	}
 
 private:
--- MathTest/KernelTest.cpp
+++ MathTest/KernelTest.cpp
@@ -4,9 +4,12 @@
 
 using namespace Crystal::Math;
 
-TEST(Kernel1dTest, Test)
+TEST(Kernel1dTest, TestGet)
 {
-	Gaussian1d<float> k;
-	const auto expected = 1.0 / std::sqrt(2.0 * Tolerance<float>::getPI());
-	EXPECT_FLOAT_EQ(expected, k.get(0));
+	using T = float;
+	Gaussian<T> k;
+	const auto expected = T(1) / std::sqrt(T(2) * Tolerance<float>::getPI());
+	EXPECT_FLOAT_EQ(expected, k.get(T(0)) );
+	EXPECT_FLOAT_EQ(expected*2, k.get(T(0),T(0)));
+	EXPECT_FLOAT_EQ(expected*3, k.get(T(0), T(0), T(0)));
 }
