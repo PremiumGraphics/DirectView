a2f7ce18c75c2188ba7ff03b183cf67cfdf3469f
SatoshiMabuchi <mabuchi@premiumgraphics.co.jp>
2015-06-08 17:36:22 +0900

    ファイル名のミスを修正．

---
+++ Command/BitSpaceTransformCommandTest.cpp
@@ -0,0 +1,14 @@
+#include "gtest/gtest.h"
+
+#include "BitSpaceTransformCommand.h"
+
+using namespace Crystal::Math;
+using namespace Crystal::Command;
+
+TEST(BitSpaceTransformCommandTest, TestNot)
+{
+	BitSpaceTransformCommand command;
+	const BitSpace3dSPtr<float>& space = std::make_shared<BitSpace3d<float> >();
+	command.add(space);
+	command.not();
+}
